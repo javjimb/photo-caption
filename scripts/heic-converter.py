a072b0fe2a8f64f798c0a65324329ced77137e22
Javier Jimenez Blasco <javjimb@gmail.com>
2024-05-12 07:06:13 +0200

    Add comments

--- scripts/heic-converter.py
+++ scripts/heic-converter.py
@@ -1,27 +1,42 @@
 import os
 import subprocess
 
+
 def heic_to_jpg(heic_file, jpg_file):
-    try:
-        subprocess.run(['magick', 'convert', heic_file, jpg_file], check=True)
-        os.remove(heic_file)
-        print(f"{heic_file} converted and original file deleted.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error converting {heic_file} to JPG: {e}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  """
+  Convert a HEIC file to JPG format using ImageMagick's `convert` command.
+
+  Args:
+    heic_file (str): The path to the HEIC file.
+    jpg_file (str): The path to save the converted JPG file.
+
+  Raises:
+    subprocess.CalledProcessError: If the conversion process fails.
+    Exception: If any other error occurs during the conversion.
+
+  Returns:
+    None
+  """
+  try:
+    subprocess.run(['magick', 'convert', heic_file, jpg_file], check=True)
+    os.remove(heic_file)
+    print(f"{heic_file} converted and original file deleted.")
+  except subprocess.CalledProcessError as e:
+    print(f"Error converting {heic_file} to JPG: {e}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 source_path = 'data/heic'
 destination_path = 'data/images/jpg'
 
 for file in os.listdir(source_path):
-    source_file_path = os.path.join(source_path, file)
-    destination_file_path = os.path.join(destination_path, file.split('.')[0] + '.jpg')
+  source_file_path = os.path.join(source_path, file)
+  destination_file_path = os.path.join(destination_path, file.split('.')[0] + '.jpg')
 
-    if file.lower().endswith('.heic'):
-        heic_to_jpg(source_file_path, destination_file_path)
-    elif file.lower().endswith('.jpg') or file.lower().endswith('.jpeg'):
-        os.rename(source_file_path, destination_file_path)
+  if file.lower().endswith('.heic'):
+    heic_to_jpg(source_file_path, destination_file_path)
+  elif file.lower().endswith('.jpg') or file.lower().endswith('.jpeg'):
+    os.rename(source_file_path, destination_file_path)
 
 print('HEIC to JPG conversion completed!')
 
