ec4d7d78de5dc5587ae893ef5f7e64d0f3f95867
Javier Jimenez Blasco <javjimb@gmail.com>
2024-06-23 08:42:07 +0200

    Adjust response of refined caption

--- api/services/caption_refiner.py
+++ api/services/caption_refiner.py
@@ -9,7 +9,15 @@ class CaptionRefiner:
 
     def refine_caption(self, pet_name: str, general_caption: str) -> str:
         # Construct the prompt for the model
-        prompt = f"Pet: {pet_name}\nCaption: {general_caption}\nRefined Caption:"
+        # prompt = f"Pet: {pet_name}\nCaption: {general_caption}\nRefined Caption:"
+        
+        prompt = f"""### Instruction:
+        Generate a caption for a photo of {pet_name}.
+        ### Input:
+        {general_caption}
+        ### Response:
+        """
+
         inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
         
         # Generate refined caption
@@ -23,4 +31,13 @@ class CaptionRefiner:
         )
 
         refined_caption = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return refined_caption
+        return self.extract_response(refined_caption)
+    
+    @staticmethod
+    def extract_response(caption: str) -> str:
+        response_start = caption.find('### Response:')
+        if response_start == -1:
+            return ''  # Return empty string or handle accordingly
+        
+        response = caption[response_start + len('### Response:'):].strip()
+        return response
